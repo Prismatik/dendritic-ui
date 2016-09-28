fb67a8443c288bb846c842da758292b98f14b303
Toby Flemming <toby.flemming@williamhill.com.au>
2016-09-28 15:08:28 +1000

    Change references of Input.js to input.js

--- stories/components/input.js
+++ stories/components/input.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
-import Input from '../../src/components/Input';
+import Input from '../../src/components/input';
 
 storiesOf('<Input />', module)
   .add('text', () => (
