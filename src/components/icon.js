6173d28018777deebddddf952c4520650fb05edb
Charlotte McCarthy <charlotteimccarthy@gmail.com>
2016-09-30 10:06:13 +1000

    Rename function

--- src/components/icon.js
+++ src/components/icon.js
@@ -2,15 +2,16 @@ import React, { PropTypes } from 'react';
 import styles from 'font-awesome/css/font-awesome.css';
 import cssModules from '../lib/react_css_modules';
 
-const Icon = ({ type }) => {
-  const animal = (type) => {
-    if (!styles[`fa-${type}`]) {
-      return 'bug';
-    }
-    return type;
-  };
-  return <i styleName={`fa fa-${animal(type)}`} />;
+const validIcon = (type) => {
+  if (!styles[`fa-${type}`]) {
+    return 'bug';
+  }
+  return type;
 };
+
+const Icon = ({ type }) =>
+  <i styleName={`fa fa-${validIcon(type)}`} />;
+
 
 Icon.propTypes = {
   type: PropTypes.string.isRequired
